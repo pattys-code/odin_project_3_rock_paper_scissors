5620f391b21b6510a89148620be59154713f2a1c
Patricia <162380840+pattys-code@users.noreply.github.com>
2026-04-22 21:43:40 +0200

    addbuttons for choices, show current score and final result in browser

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
+const btnRock = document.querySelector('.btn-rock');
+const btnPaper = document.querySelector('.btn-paper');
+const btnScissors= document.querySelector('.btn-scissors');
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('.result');
+const current = document.querySelector('.current');
 let humanScore = 0;
 let computerScore = 0;
 
@@ -11,10 +17,10 @@ function getComputerChoice() {
         {return 'scissors';}
 }
 
-function getHumanChoice(){
+/*function getHumanChoice(){
     let choice = prompt("Rock, Paper or Scissors?");
     return choice.toLowerCase();
-}
+}*/
 
 // Plays the game for a single round.
 //After each round, the winner is announced.
@@ -25,19 +31,20 @@ function playRound(humanChoice, computerChoice) {
     else if ((humanChoice==='rock' && computerChoice==='paper')
             || (humanChoice==='paper' && computerChoice==='scissors')
             || (humanChoice==='scissors' && computerChoice==='rock')){
-        console.log("You lose this round.");
+        //console.log("You lose this round.");
         computerScore++;
     }
     else {
-        console.log("You win this round.");
+        //console.log("You win this round.");
         humanScore++;
     }
-}
+};
+
 
 // Plays the game for a specified amount of rounds.
 // After each round, the current winner and score are announced.
 // After the rounds are done, the final winner is announced.
-function playGame(rounds) {
+/*function playGame(rounds) {
     for (let round=1; round<rounds+1;round++) {
         console.log('ROOUND '+round);
         const humanChoice = getHumanChoice();
@@ -58,9 +65,28 @@ function playGame(rounds) {
     else {
         console.log("You lose!");
     }
-}
+}*/
+
+
+buttons.forEach(button =>{
+    button.addEventListener('click', () => {
+        let humanChoice = button.id;
+        playRound(humanChoice, getComputerChoice());
+        current.textContent = 'Current score (You/Computer): '+humanScore+'/'+computerScore;
+        if (humanScore==5 || computerScore==5) {
+            buttons.forEach(button => {
+                button.disabled = true;
+            });
+            if (computerScore<5) {
+                result.textContent = "FINAL: WIN";
+            } else if (humanScore<5 ) {
+                result.textContent = "FINAL: LOSE";
+            } else {
+                result.textContent = "FINAL: TIE";
+            }
+        }
+})});
 
 // Play game for 5 rounds.
-playGame(5);
-
+//playGame(5);
 
